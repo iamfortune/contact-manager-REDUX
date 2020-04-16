33ebc67f9c3049a544076f6816be7ee03ccfd8ec
iamfortune <ikechifortune@gmail.com>
2020-04-16 01:30:16 +0100

    added other functions and actions for add and delete contacts

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -5,3 +5,17 @@ export const getContacts = () => {
         type: GET_CONTACTS
     };
 };
+
+export const deleteContact = (id) => {
+  return {
+    type: DELETE_CONTACT,
+    payload: id
+  };
+};
+
+export const addContact = (contact) => {
+  return {
+    type: ADD_CONTACT,
+    payload: contact
+  };
+};
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,3 @@
 export const GET_CONTACTS = 'GET_CONTACTS';
+export const DELETE_CONTACTS = 'DELETE_CONTACTS';
+export const ADD_CONTACTS = 'ADD_CONTACTS';
